d8f7df1b4f92a10a24c2106238f54d776e944f27
Manuzinha <00001118610106sp@al.educacao.sp.gov.br>
2024-08-24 17:00:20 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  
+   function draw() {
+    background(220);
+    createCanvas(600, 600);
+  }
+  
+  function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+  }
+  function setup() {
+    createCanvas(600, 600);
+  }
